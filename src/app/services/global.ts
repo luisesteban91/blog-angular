1a37db0c0039ffe49977306d2e500638c8813ca4
unknown <luisesteban.cch@gmail.com>
2019-06-19 22:51:39 -0500

    modificacion de URL Globa

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var global = {
-	url: 'http://localhost:8888/master-fullstack/api-rest-laravel/public/api/'
-	//url: 'http://api-rest-laravel.com.devel/api/'
+	//url: 'http://localhost:8888/master-fullstack/api-rest-laravel/public/api/'
+	url: 'http://api-rest-laravel.com.devel/api/'
 }
